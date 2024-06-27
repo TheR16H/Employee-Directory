fe5c414f14b0c1ccc9f35274c046262f360ea64e
TheR16H <rashawn5hall@gmail.com>
2024-06-26 21:44:51 -0400

    started sqls

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,18 @@
-C
+CREATE DATABASE rashawn_db;
+
+\c rashawn_db;
+
+CREATE TABLE department (
+
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+)
+
+role
+id --same
+title --same as name
+salary DECIAML NOT NULL
+
+employee 
+id --same
+first name 
