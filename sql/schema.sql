f0399bc3895addd2d4ac090984ddfb5677d2d6fd
TheR16H <rashawn5hall@gmail.com>
2024-06-27 11:10:11 -0400

    added commas and finished all table setups

--- sql/schema.sql
+++ sql/schema.sql
@@ -7,34 +7,18 @@ CREATE TABLE department (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
--- name: to hold department name
 
--- -- -- -- -- -- -- -- -- --
 
 CREATE TABLE role (
-id SERIAL PRIMARY KEY
-title VARCHAR(30) UNIQUE NOT NULL
+id SERIAL PRIMARY KEY,
+title VARCHAR(30) UNIQUE NOT NULL,
 salary DECIAML NOT NULL
 );
--- title: to hold role title
--- salary:to hold role salary
--- department_id - to hold reference to department role belongs to
-
-
--- -- -- -- -- -- -- -- -- --
 
 CREATE TABLE employee (
-id --same
-first name 
+id SERIAL PRIMARY KEY,
+first_name: VARCHAR(30) NOT NULL,
+last_name: VARCHAR(30) NOT NULL,
+role_id: INT NOT NULL,
+manager_id: INT
 );
- `employee`
-
-  * `id`: `SERIAL PRIMARY KEY`
-
-  * `first_name`: `VARCHAR(30) NOT NULL` to hold employee first name
-
-  * `last_name`: `VARCHAR(30) NOT NULL` to hold employee last name
-
-  * `role_id`: `INTEGER NOT NULL` to hold reference to employee role
-
-  * `manager_id`: `INTEGER` to hold reference to another employee that is the manager of the current employee (`null` if the employee has no manager)
