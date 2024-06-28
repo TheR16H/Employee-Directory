49d1abf7d52ebc577644eb0657673f6d9f1109ba
TheR16H <rashawn5hall@gmail.com>
2024-06-27 23:08:20 -0400

    testing seed code

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,11 +1,11 @@
 -- some pre-establish date will go here to round out
 -- each table
 
-INSERT INTO department (first_name, last_name) 
+INSERT INTO department (first_name, last_name, salary) 
 VALUES ('Marketing');
 
-INSERT INTO department (first_name, last_name)
+INSERT INTO department (first_name, last_name, salary)
  VALUES ('Sales');
 
-INSERT INTO department (first_name, last_name) 
+INSERT INTO department (first_name, last_name, salary) 
 VALUES ('Human Resources');
