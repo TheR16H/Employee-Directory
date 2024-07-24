1c04ebee235a869070d2b517ce0dbd54e7897de6
TheR16H <rashawn5hall@gmail.com>
2024-07-24 11:40:33 -0400

    readjusted and removed listening port since theres no front end

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const inquirer = require('inquirer');
 const express = require('express');
+const { Pool } = require('pg');
 const { getNewEmployee, getNewRole, getNewDepartment, updateEmployee } = require('./sql');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
 const pool = new Pool({ user: 'postgres', password: 'micha', host: 'localhost', database: 'employee' });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const questions = [{ type: 'list', 
+const questions = [{ 
+    type: 'list', 
     message: 'What would you like to do?', 
     name: 'choice', 
-    choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View all Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'] 
-}];
+    choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View all Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'] }];
 
 pool.connect();
