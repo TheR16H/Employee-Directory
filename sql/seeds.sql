84641d4e59d29519ffa7173ac35535fd0fe1d504
TheR16H <rashawn5hall@gmail.com>
2024-07-24 10:34:51 -0400

    worked on the seeds and addded and outline

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -2,10 +2,20 @@
 -- each table
 
 INSERT INTO department (first_name, last_name, salary) 
-VALUES ('Marketing');
-('Sales')('Human Resources')('Human Resources')
-INSERT INTO role (first_name, last_name, salary)
- VALUES ;
+VALUES ('Marketing'),
+('Sales'),
+('Human Resources'),
+('Human Resources');
+INSERT INTO role (title, salary, department)
+ VALUES (f, 2, 1),
+        (f, 2, 2),
+        (f, 2, 3),
+        (f, 2, 3),
+        (f, 2, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-VALUES ;
+VALUES  (f, l, 2, 1),
+        (f, l, 3, 2),
+        (f, l, 1, NULL),
+        (f, l, 4, NULL),
+        (f, l, 2, NULL);
