043f0517b20239bf93e76de645f885344526644b
TheR16H <rashawn5hall@gmail.com>
2024-07-24 10:29:02 -0400

    actually worked on the seeds

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -3,9 +3,9 @@
 
 INSERT INTO department (first_name, last_name, salary) 
 VALUES ('Marketing');
+('Sales')('Human Resources')('Human Resources')
+INSERT INTO role (first_name, last_name, salary)
+ VALUES ;
 
-INSERT INTO department (first_name, last_name, salary)
- VALUES ('Sales');
-
-INSERT INTO department (first_name, last_name, salary) 
-VALUES ('Human Resources');
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES ;
