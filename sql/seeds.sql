b3c00538b8e8942141e45bea08d0e085ac5e91e2
TheR16H <rashawn5hall@gmail.com>
2024-07-24 10:58:28 -0400

    added notes and further filled out seeds. finished tables 1

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -5,17 +5,24 @@ INSERT INTO department (first_name, last_name, salary)
 VALUES ('Marketing'),
 ('Sales'),
 ('Human Resources'),
-('Human Resources');
+('EVP');
 INSERT INTO role (title, salary, department)
- VALUES (f, 2, 1),
-        (f, 2, 2),
-        (f, 2, 3),
-        (f, 2, 3),
-        (f, 2, 4);
+ VALUES ('VP Branding', 9000000, 1),
+        ('Director of Advertising', 7500000, 2),
+        ('Intern', 50000, 3),
+        ('Intern', 50001, 3),
+        ('Social media assistant',60000 , 4); 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-VALUES  (f, l, 2, 1),
-        (f, l, 3, 2),
-        (f, l, 1, NULL),
-        (f, l, 4, NULL),
-        (f, l, 2, NULL);
+VALUES  ('f', 'l', 3, NULL),
+        ('f', 'l', 3, NULL),
+        ('f', 'l', 1, 1),
+        ('f', 'l', 2, 2),
+        ('f', 'l', 4, NULL);
+--  ^ this set up is for if manager id means they are manager
+--  v this set up is for if its the opposite
+        -- ('f', 'l', 3, 1), 
+        -- ('f', 'l', 3, 2),
+        -- ('f', 'l', 1, NULL),
+        -- ('f', 'l', 2, NULL),
+        -- ('f', 'l', 4, NULL);
