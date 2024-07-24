d75aba918ee656cd0e3e53596b0df9c0906b944f
TheR16H <rashawn5hall@gmail.com>
2024-07-24 11:25:36 -0400

    filled out seeds

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,11 +1,11 @@
 -- some pre-establish date will go here to round out
 -- each table
 
-INSERT INTO department (first_name, last_name, salary) 
-VALUES ('Marketing'),
-('Sales'),
-('Human Resources'),
-('EVP');
+INSERT INTO department (name) 
+VALUES  ('Marketing'),
+        ('Sales'),
+        ('Human Resources'),
+        ('VP');
 INSERT INTO role (title, salary, department)
  VALUES ('VP Branding', 9000000, 1),
         ('Director of Advertising', 7500000, 2),
@@ -14,15 +14,15 @@ INSERT INTO role (title, salary, department)
         ('Social media assistant',60000 , 4); 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-VALUES  ('f', 'l', 3, NULL),
-        ('f', 'l', 3, NULL),
-        ('f', 'l', 1, 1),
-        ('f', 'l', 2, 2),
-        ('f', 'l', 4, NULL);
+VALUES  ('Jess', 'Bowen', 3, NULL),
+        ('Robert', 'Dawson', 3, NULL),
+        ('Syxx', 'Singer', 1, 1),
+        ('Jewel', 'Reyes', 2, 2),
+        ('Tom', 'Fletcher', 4, NULL);
 --  ^ this set up is for if manager id means they are manager
 --  v this set up is for if its the opposite
-        -- ('f', 'l', 3, 1), 
-        -- ('f', 'l', 3, 2),
-        -- ('f', 'l', 1, NULL),
-        -- ('f', 'l', 2, NULL),
-        -- ('f', 'l', 4, NULL);
+        -- ('Jess', 'Bowen', 3, 1), 
+        -- ('Robert', 'Dawson', 3, 2),
+        -- ('Syxx', 'Singer', 1, NULL), manager
+        -- ('Jewel', 'Reyes', 2, NULL), manager
+        -- ('Tom', 'Fletcher', 4, NULL);
