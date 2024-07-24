b7627cf58a7e7a117faef666cac8ee14ac65c265
TheR16H <rashawn5hall@gmail.com>
2024-07-24 13:29:23 -0400

    rough copy of function #1 to handle the question answers

--- index.js
+++ index.js
@@ -16,3 +16,34 @@ const questions = [{
     choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View all Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'] }];
 
 pool.connect();
+function handleUserChoice(choice) {
+    let sql = '';
+    switch (choice) {
+        case 'View All Employees':
+            sql = `SELECT e.id, e.first_name, e.last_name, r.department, r.salary, e.manager_id, d.name FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON r.department = d.id`;
+            break;
+        case 'Add Employee':
+            getNewEmployee().then(sql => executeQuery(sql));
+            break;
+        case 'Update Employee Role':
+            updateEmployee().then(sql => executeQuery(sql));
+            break;
+        case 'View all Roles':
+            sql = 'SELECT * FROM role';
+            break;
+        case 'Add Role':
+            getNewRole().then(sql => executeQuery(sql));
+            break;
+        case 'View All Departments':
+            sql = 'SELECT * FROM department';
+            break;
+        case 'Add Department':
+            getNewDepartment().then(sql => executeQuery(sql));
+            break;
+        default:
+            process.exit();
+    }
+    if (sql) {
+        executeQuery(sql);
+    }
+}
