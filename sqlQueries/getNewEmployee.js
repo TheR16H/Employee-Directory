fe1d7e1adb8c8e4e911bb458a7f4b55b169f6bbd
TheR16H <rashawn5hall@gmail.com>
2024-08-04 10:20:43 -0400

    Started newEmployee file based off my newDepartment structure

--- sqlQueries/getNewEmployee.js
+++ sqlQueries/getNewEmployee.js
@@ -1,1 +1,14 @@
+const inquirer = require('inquirer');
+
+function getNewEmployee() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter the first name of the new employee:',
+            name: 'firstName',
+            validate: function (input) {
+                if (input.trim() === '') {
+                    return 'First name cannot be empty. Please enter a valid first name.';
+                }
+                return true;
 module.exports = getNewEmployee;
